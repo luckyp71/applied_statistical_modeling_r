fbf3cb0f0833d3ad5335497f892e5736343d2cd4
luckyp71 <luckypratama71@yahoo.com>
2025-07-24 21:11:35 +0700

    update

--- exp11.R
+++ exp11.R
@@ -15,7 +15,7 @@ fit1 <- lm(petal_length ~ sepal_length, data=data)
 summary(fit1) # the p-value is much lower than 0.05, so we can say that this is essentially statistically significant, and this model explain 75.83% variation in petal length
 # the intercept: -7.10 and slope 1.85843
 
-# multiple regression
+# multiple linear regression
 fit2 <- lm(petal_length ~ .-species, data=data) # exclude species
 summary(fit2) # the p-value of intercept is greater than 0.05, meaning it's not statistically significant, so we can exclude it in our linear regression equation
 # petal length (y) = 0.73*(sepal_length) - 0.65*(sepal_width) + 1.44*(petal_width)
